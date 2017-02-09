09e8e21d7e30cc1507cd25ae679c198ffedaa5ed
masterimpaler <loughlinjon@gmail.com>
2017-02-09 17:53:17 +0300

    added more fields on org signup form

--- resources/views/organization/create.blade.php
+++ resources/views/organization/create.blade.php
@@ -17,10 +17,39 @@
                         </div>
 
                         <div class="form-group">
-                            {!! Form::label('Description', 'Description') !!}
-                            {!! Form::text('description', '', ['class' => 'form-control']) !!}
+                            {!! Form::label('Date Founded', 'Date Founded') !!}
+                            {!! Form::date('date founded', \Carbon\Carbon::now()) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('Location', 'Location') !!}
+                            {!! Form::text('location', '', ['class' => 'form-control']) !!}
                         </div>
 
+                        <div class="form-group">
+                            {!! Form::label('Description', 'Description') !!}
+                            {!! Form::textarea('description', '', ['class' => 'form-control']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Image', 'Image') !!}
+                            {!! Form::file('image', '', ['class' => 'form-control']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Upload File', 'Upload File') !!}
+                            {!! Form::file($organization, $attributes = []) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Organization Type', 'Organization Type') !!}
+                            {!! Form::radio('name', 'Profit') !!}
+                            {!! Form::radio('name', 'Non Profit') !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('Contact Email', 'Contact Email') !!}
+                            {!! Form::label('email', 'E-Mail Address', ['class' => 'awesome']) !!}
+                        </div>
                         <button class="btn btn-success" type="submit">Create Organization!</button>
 
                         {!! Form::close() !!}
