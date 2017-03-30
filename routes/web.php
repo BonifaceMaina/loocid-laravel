ef03cacae7ea984584dd66ef114a8c64fda6775b
Boniface <karisbm@gmail.com>
2017-03-30 10:13:42 +0300

    Changes to organizations.blade.php

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,7 @@ Route::get('/user/activation/{token}', 'Auth\RegisterController@userActivation')
 Route::get('/organizations', function (){
    return view('organizations');
 });
-<<<<<<< HEAD
-=======
+
 
 //Projects routes
 Route::resource('/project', 'ProjectsController');
@@ -50,4 +49,3 @@ Route::get('/share', function()
     //return Share::load('http://www.example.com', 'Link description')->services('facebook', 'gplus', 'twitter');
 
 });
->>>>>>> 6b1127bb64f32edcbeeeecdf8957eb4fd71c2e37
